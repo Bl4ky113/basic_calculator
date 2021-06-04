fd0248b7c9ed1c66746efd7e5911a040052f119f
Bl4ky113 <martinstevenhernandezortiz@gmail.com>
2021-06-03 19:01:39 -0500

    Agregado Calculo a la Calculadora, verificacion de errores y arreglo de operaciones Math.sqrt

--- src/js/dist/script.dev.js
+++ src/js/dist/script.dev.js
@@ -5,7 +5,8 @@
 var get = {
   id: document.getElementById.bind(document),
   "class": document.getElementsByClassName.bind(document),
-  tag: document.getElementsByTagName.bind(document)
+  tag: document.getElementsByTagName.bind(document),
+  query: document.querySelector.bind()
 };
 /* OutPut */
 
@@ -39,24 +40,77 @@ acBtn.onclick = function () {
 var calcBtn = get.id("btn_solve");
 
 calcBtn.onclick = function () {
-  checkValue();
+  return checkValue();
 };
 
+var operationsArr = [];
+
 function addValue(obj) {
-  var value = obj.innerText;
+  var value = obj.innerText; // Correct the simbol of EXP
 
-  if (Number.isInteger(value)) {}
+  if (obj.innerText == "[]²") value = "&sup2;"; // Add Value to Operations Arr
 
+  if (Number.isInteger(parseInt(obj.value))) operationsArr.push(parseInt(obj.value));else operationsArr.push(obj.value);
+  console.log(operationsArr);
+  if (output.innerHTML == "error") output.innerHTML = "";
   output.innerHTML += value;
 }
 
 function delValue(condition) {
   if (condition == false) {
     var deleted = output.innerHTML.slice(0, -1);
+    operationsArr.pop();
     output.innerHTML = deleted;
   } else {
+    operationsArr = [];
     output.innerHTML = "";
   }
 }
 
-function checkValue() {}
+function search(array, keyword) {
+  var search_result = [];
+  array.forEach(function (element, index) {
+    if (keyword == element) search_result.push(index);
+  });
+  return search_result;
+}
+
+function checkValue() {
+  var formula = operationsArr.join(""); // Search and Complete Sqrt functions
+
+  var search_result = search(operationsArr, "Math.sqrt");
+  var rootNum = [];
+
+  for (var _i = 0; _i < search_result.length; _i += 1) {
+    var rootNum_index = [];
+    var index = search_result[_i] + 1;
+
+    while (index < operationsArr.length) {
+      var is_operator = operationsArr[index] == "+" || operationsArr[index] == "-" || operationsArr[index] == "*" || operationsArr[index] == "/" || operationsArr[index] == "**";
+      operationsArr[index] == "";
+      operationsArr[index] == undefined;
+
+      if (is_operator) {
+        break;
+      }
+
+      rootNum_index.push(operationsArr[index]);
+      index += 1;
+    }
+
+    rootNum.push(rootNum_index.join(""));
+    var toRemplace = "Math.sqrt".concat(rootNum[_i]);
+    var replacement = "Math.sqrt(".concat(rootNum[_i], ")");
+    formula = formula.replace(toRemplace, replacement);
+  } // Calculate the Result
+
+
+  try {
+    var result = eval(formula);
+  } catch (error) {
+    result = "error";
+    operationsArr = [];
+  } finally {
+    output.innerHTML = result;
+  }
+}
--- src/js/script.js
+++ src/js/script.js
@@ -4,7 +4,8 @@
 const get = {
   id: document.getElementById.bind(document),
   class: document.getElementsByClassName.bind(document),
-  tag: document.getElementsByTagName.bind(document)
+  tag: document.getElementsByTagName.bind(document),
+  query: document.querySelector.bind()
 };
 
 /* OutPut */
@@ -25,15 +26,22 @@ acBtn.onclick = () => delValue(true);
 
 /* Check/Calculate/Equals Button */
 const calcBtn = get.id("btn_solve");
-calcBtn.onclick = () => {
-  checkValue();
-}
+calcBtn.onclick = () => checkValue();
+
+var operationsArr = [];
 
 function addValue (obj) {
-  let value = obj.innerText;
-  if (Number.isInteger(value)) {
-    
-  }
+  var value = obj.innerText;
+  
+  // Correct the simbol of EXP
+  if (obj.innerText == "[]²") value = "&sup2;";
+
+  // Add Value to Operations Arr
+  if (Number.isInteger(parseInt(obj.value))) operationsArr.push(parseInt(obj.value));
+  else operationsArr.push(obj.value);
+  console.log(operationsArr)
+
+  if (output.innerHTML == "error") output.innerHTML = "";
 
   output.innerHTML += value;
 }
@@ -41,12 +49,59 @@ function addValue (obj) {
 function delValue (condition) {
   if (condition == false) {
     let deleted = output.innerHTML.slice(0, -1);
+    operationsArr.pop();
     output.innerHTML = deleted;
   } else {
+    operationsArr = [];
     output.innerHTML = "";
   }
 }
 
+function search (array, keyword) {
+  let search_result = [];
+  array.forEach( (element, index) => {
+    if (keyword == element) search_result.push(index);
+  });
+  return search_result;
+}
+
 function checkValue () {
+  var formula = operationsArr.join("");
+
+  // Search and Complete Sqrt functions
+  let search_result = search(operationsArr, "Math.sqrt");
+  let rootNum = [];
+  for (let i = 0; i < search_result.length; i += 1) {
+    let rootNum_index = [];
+    let index = search_result[i] + 1;
+
+    while (index < operationsArr.length) {
+      let is_operator = operationsArr[index] == "+" ||
+                        operationsArr[index] == "-" ||
+                        operationsArr[index] == "*" ||
+                        operationsArr[index] == "/" ||
+                        operationsArr[index] == "**";
+                        operationsArr[index] == "";
+                        operationsArr[index] == undefined;
+      if (is_operator) {break;}
+
+      rootNum_index.push(operationsArr[index]);
+      index += 1;
+    }
+    rootNum.push(rootNum_index.join(""));
+
+    let toRemplace = `Math.sqrt${rootNum[i]}`;
+    let replacement = `Math.sqrt(${rootNum[i]})`;
+    formula = formula.replace(toRemplace, replacement);
+  }
   
+  // Calculate the Result
+  try {
+    var result = eval(formula);
+  } catch (error) {
+    result = "error";
+    operationsArr = [];
+  } finally {
+    output.innerHTML = result;
+  }
 }
